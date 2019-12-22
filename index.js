cf0991125e4402619aea527b1f6f6e7a146f4167
Pradeep <pradeep99909@gmail.com>
2019-12-22 19:05:28 +0530

    ee

--- index.js
+++ index.js
@@ -51,15 +51,13 @@ app.post('/getcity',(req,res)=>{
     })
 })
 
-// exports.get = function(event, context, callback) {
-//   var contents = fs.readFileSync(`public${path.sep}index.html`);
-//   var result = {
-//     statusCode: 200,
-//     body: contents.toString(),
-//     headers: {'content-type': 'text/html'}
-//   };
+exports.get = function(event, context, callback) {
+  var contents = fs.readFileSync(`public${path.sep}index.html`);
+  var result = {
+    statusCode: 200,
+    body: contents.toString(),
+    headers: {'content-type': 'text/html'}
+  };
 
-// };
+};
 
-
-exports.app;
