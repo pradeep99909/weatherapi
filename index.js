52d0dfbaf77bfbd0062ae154347c6b3ed221be62
Pradeep <pradeep99909@gmail.com>
2019-12-22 19:28:49 +0530

    modules exports app

--- index.js
+++ index.js
@@ -38,13 +38,16 @@ app.post('/getcity',(req,res)=>{
     })
 })
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
-  app()
-};
+// exports.get = function(event, context, callback) {
+//   var contents = fs.readFileSync(`public${path.sep}index.html`);
+//   var result = {
+//     statusCode: 200,
+//     body: contents.toString(),
+//     headers: {'content-type': 'text/html'}
+//   };
+//   app()
+// };
+
+
+module.exports = app
 
